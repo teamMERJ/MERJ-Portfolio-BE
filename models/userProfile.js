2059db22893da0bc255f56de2dfda63bc682ad3b
japhlett <jatakyiwaa001@st.ug.edu.gh>
2024-07-10 10:16:05 +0000

    Broke up user model into individual models

---
+++ models/userProfile.js
@@ -0,0 +1,23 @@
+import { Schema, Types, model } from "mongoose";
+import { toJSON } from "@reis/mongoose-to-json";
+
+const userProfileSchema = new Schema({
+    profilePicture: { type: String },
+    location: { type: String },
+    maritalStatus: {
+      type: String,
+      enum: ["single", "married", "prefer-not-to-say"]},
+    sex: { type: String, enum: ["male", "female"] },
+    bio: { type: String },
+    about: { type: String },
+    dateOfBirth: { type: Date },
+    contact: { type: String },
+    cv: { type: String },
+    languages: [{ type: String }],
+    gitHubLink: { type: String },
+    linkedInlink: { type: String },
+    user: {type:Types.ObjectId, ref: 'User'}
+});
+
+userProfileSchema.plugin(toJSON);
+export const UserProfile = model("UserProfile", userProfileSchema);

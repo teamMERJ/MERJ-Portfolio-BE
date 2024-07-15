e223b246fdc08cd181374c9afaea7eb81d2eb6f1
japhlet <jatakyiwaa001@st.ug.edu.gh>
2024-07-15 13:50:08 +0000

    Update userProfile.js

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -3,15 +3,6 @@ import { createUserProfile, deleteUserProfile, getUserProfile, updateUserProfile
 import { checkUserSession } from "../middlewares/auth.js";
 import { remoteUpload } from "../middlewares/uploads.js";
 
-export const profileRouter = Router();
-
-profileRouter.post('/users/profiles', checkUserSession, postUserProfile);
-profileRouter.get('//users/profiles', checkUserSession, getAllProfile);
-profileRouter.get('/users/profiles/:id', checkUserSession, getOneProfile);
-profileRouter.patch('/users/profiles/:id', checkUserSession, updateProfile);
-profileRouter.delete('/users/profiles /:userProfileId', checkUserSession, deleteUserProfile);
-
-
 
 export const profileRouter = Router();
 
